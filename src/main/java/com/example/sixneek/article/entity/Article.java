d07cccb40bb228c8cc5830db7810234e81fa6336
Park Young jun <jungin598@gmail.com>
2023-07-27 09:37:37 +0900

    Chore: 스케줄링 기능 crawling 로직에 합침, Article id 직접 생성, Remove: Scheduler 패키지 삭제

--- src/main/java/com/example/sixneek/article/entity/Article.java
+++ src/main/java/com/example/sixneek/article/entity/Article.java
@@ -1,7 +1,6 @@
 package com.example.sixneek.article.entity;
 
 
-import com.example.sixneek.article.dto.ArticleRequestDto;
 import com.example.sixneek.like.entity.Like;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -19,7 +18,7 @@ import java.util.List;
 @Entity
 public class Article {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String image;
     private String title;
@@ -31,14 +30,4 @@ public class Article {
     @Builder.Default
     @OneToMany(mappedBy = "article", cascade = CascadeType.REMOVE)
     private List<Like> likeList = new ArrayList<>();
-
-    // Scheduler : 기사 업데이트
-    public void updateByArticleRequestDto(ArticleRequestDto articleRequestDto) {
-        this.id = articleRequestDto.getId();
-        this.image = articleRequestDto.getImage();
-        this.title = articleRequestDto.getTitle();
-        this.date = articleRequestDto.getDate();
-        this.content = articleRequestDto.getContent();
-        this.tag = articleRequestDto.getTag();
-    }
 }
--- src/main/java/com/example/sixneek/crawling/CrawlingArticle.java
+++ src/main/java/com/example/sixneek/crawling/CrawlingArticle.java
@@ -4,10 +4,12 @@ import com.example.sixneek.article.dto.ArticleListResponseDto;
 import com.example.sixneek.article.entity.Article;
 import com.example.sixneek.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -15,24 +17,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j(topic = "Scheduler")
 @RequiredArgsConstructor
 public class CrawlingArticle {
     private final ArticleRepository articleRepository;
 
     // 크롤링: 전체 기사 조회
+    @Scheduled(cron = "0 0 */2 * * *")      // 2시간 마다 기사 업데이트
     public List<ArticleListResponseDto> saveArticleList() throws IOException {
+        articleRepository.deleteAll();
         List<ArticleListResponseDto> articleResponseDtoList = new ArrayList<>();
-
+        long currentId = 1;
         for (int i = 1; i <= 10; i++) {
             String Article_URL = "https://www.hani.co.kr/arti/list" + i + ".html";
-            articleResponseDtoList.addAll(processUrl(Article_URL));
+            articleResponseDtoList.addAll(processUrl(Article_URL, currentId));
+            currentId += 15;
         }
+
+        log.info("뉴스 기사 크롤링 완료");
 
         return articleResponseDtoList;
     }
 
     // Url 처리하기
-    private List<ArticleListResponseDto> processUrl(String url) throws IOException {
+    private List<ArticleListResponseDto> processUrl(String url, long currentId) throws IOException {
         Document document = Jsoup.connect(url).get();
         Elements contentList = document.select("div div.section-list-area div.list");
 
@@ -49,6 +57,7 @@ public class CrawlingArticle {
             String content = articleDocument.select("div div.text").text();
 
             Article article = Article.builder()
+                    .id(currentId++)
                     .image(image)
                     .title(title)
                     .tag(tag)
